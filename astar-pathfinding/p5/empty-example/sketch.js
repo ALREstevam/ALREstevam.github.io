91269375e943f3e45a3d7cc9f4cb9240a59b6dd6
ALREstevam <a166348@g.unicamp.com>
2018-05-21 10:22:17 -0300

    proj jp add saved & astar update

--- astar-pathfinding/p5/empty-example/sketch.js
+++ astar-pathfinding/p5/empty-example/sketch.js
@@ -1,5 +1,25 @@
 "use strict";
 
+/*
+* Este algoritmo gera um mapa e usa de algum algoritmo de busca com ou sem heurística para encontrar um caminho
+* (caso exista) entre um nó inicial e um nó final.
+*
+* O mapa gerado é dividido entre a parte verde (simulando solo) e a parte azul (simulando água),
+* o agente precisa encontrar o caminho entre dois nós do grid com a limitação de não poder passar pela água
+* os caminhos por terra estão representados em diferentes tons de verde - tons mais claros indicam caminhos mais fáceis
+* de serem percorridos e tons mais escuros, caminhos mais difíceis.
+*
+* A geração do mapa é aleatória, podendo haver mapas em que não existe caminho possível entre os dois nós, nele
+* os pedaços de terra que tocam água são mais fáceis de serem percorridos, simulando uma praia.
+*
+* Este é um código na linguagem JavaScript utilizando o framework P5.
+*
+* */
+
+
+/*
+* Remoção de um elemento de um vetor: o javascript não faz isso nativamente, é preciso codificar esse comportamento.
+* */
 function removeFromArray(arr, elem) {
     for(var i = arr.length -1; i >= 0; i--){
         if(arr[i] == elem){
@@ -8,48 +28,101 @@ function removeFromArray(arr, elem) {
     }
 }
 
-function heuristic(a, b) {
+/*Lista dos tipos de cálculo de distância entre pontos (utilizados na heurística)*/
+var distanceHeuristicTypes = ['Euclidean', 'Manhattan'];
 
-    //Euclidean
-    //var d = dist(a.x, a.y, b.x, b.y);
-    var d = abs(a.x - b.x) + abs(a.y - b.y);
-    //Manhattan
+/*Cálculo da distância entre dois nós*/
+function heuristic(a, b, distance) {
 
+    var d;
+    if(distance == distanceHeuristicTypes[0]){
+        //Euclidean distance
+        d = dist(a.x, a.y, b.x, b.y);
+    }else{
+        //Manhattan distance
+        d = abs(a.x - b.x) + abs(a.y - b.y);
+    }
     return d;
-
 }
 
+/*Contadores*/
+var stepsCount = 0;
 
-
-
+/*Tamanho do grid*/
 var cols = 60;
 var rows = cols;
 
+/*Declaração da variável que armazena o grid/grafo */
 var grid = undefined;
 
+/*Lista de nós abertos e nós fechados*/
 var openSet = [];
 var closeSet = [];
 
+/*Lista de nós no caminho escolhido*/
+var path = [];
+
+/*Início e fim*/
 var start;
 var end;
 
+/*Altura e largura de cada quadrado no mapa*/
 var w, h;
 
-var path = [];
 
+/*Variáveis que armazenam objetos do DOM (interface)*/
 var inputSize;
 
 
+/*Flag: algoritmo está em execução*/
+var execute = false;
 
-var m = 0.5; //neighbour.g -> difficulty
+
+/*======================================= VARIÁVEIS QUE MUDAM O COMPORTAMENTO DO PROGRAMA ===========================*/
+
+var m = 1; //neighbour.g -> difficulty
 var n = 1; //neighbour.h -> distance to goal
+
+/*
+* A heurística utilizada será:
+*
+* f(i) = (m * g(i)) + (n * h(i))
+*
+* Em que i é um nó
+* g(i) é a soma do caminho percorrido até o momento somada à dificuldade de ir ao nó i
+* h(i) é a distância do nó i até o nó objetivo (o método de cálculo de distância pode ser escolhido posteriormente)
+*
+* Compotamentos do algoritmo
+*
+* SE m = 1 e n = 1 ENTÃO A FUNÇÃO HEURÍSTICA SERÁ: (f(i) = g(i) + h(i)) O QUE LEVA A UMA BUSCA: A* visto que a distância
+*   até o objetivo e a dificuldade do caminho serão considerados
+* SE m = 1 e n = 0 ENTÃO A FUNÇÃO HEURÍSTICA SERÁ: (f(i) = g(i)) O QUE LEVA A UMA BUSCA:
+*   mista entre amplitude e profundidade em que os nós de menor custo serão explorados primeiro.
+* SE m = 0 e n = 1 ENTÃO A FUNÇÃO HEURÍSTICA SERÁ: (f(i) = h(i)) O QUE LEVA A UMA BUSCA:
+*   best first com backtracking: caso o algoritmo chegue em um dead end tentará continuar a partir do nó aberto com
+*   menor distância até o objetivo.
+* SE m = 0 e n = 0 ENTÃO A FUNÇÃO HEURÍSTICA SERÁ: nenhuma O QUE LEVA A UMA BUSCA: em amplitude, os nós vizinhos do nós
+*   abertos serão explorados primeiro.
+*
+*
+* */
+
+
+/*Porcentagem de barreiras em locais randômicos: chance de um nó qualquer ser uma barreira*/
 var randomWallPercentage = 0.0;
+/*Permitir que o agente se mova na diagonal*/
 var allowDiagonalMovement = true;
-var difficultPathToWallAmount = 0.6;
+/*Quantidade de barreiras geradas em grupos: quanto maior for o número maiores serão os "rios"*/
+var difficultPathToWallAmount = 0.5;
+/*Método do cálculo de distância
+* distanceHeuristicTypes[0] :  distância Euclidiana
+* distanceHeuristicTypes[1] :  distância de Manhattan
+* */
+var choosenDistanceMethod = distanceHeuristicTypes[1];
 
+/*====================================================================================================================*/
 
-
-
+/*Definção do objeto que representa cada célula do grid / nó do grafo*/
 function Cell(xpos, ypos){
     this.f = 0;
     this.g = 0;
@@ -64,8 +137,10 @@ function Cell(xpos, ypos){
     this.wall = random(1) < randomWallPercentage;
     this.supereasy = false;
     this.difficulty =  1;
+    this.inClosedSet = false;
+    this.inOpenSet = false;
 
-
+    /*Mostrar o nó de forma visual: com uma cor especidifacada como argumento*/
     this.showPath = function (clr) {
         if(this.wall){
            fill(0);
@@ -76,36 +151,46 @@ function Cell(xpos, ypos){
         rect(this.x * w, this.y * h, w, h);
     };
 
+    /*Definição das cores padrão dependendo do tipo e parâmetros do nó*/
     this.show = function () {
         var r = 1;
         var g = 1;
         var b = 1;
 
         if(this.wall){
+            //Wall
             r = 2;
             g = 204;
             b = 204;
         }else if(this.supereasy){
-            //60, 255, 53
+            //Supereasy cell
             r = 60;
             g = 255;
             b = 53;
         }
         else{
+            //Normal cell
             r = 32;
             g = 255 - ((this.difficulty * 3) % 200);
             b = 0;
-
-
-
         }
+        if(this.inClosedSet){
+            r += 150;
+        }else
+        if(this.inOpenSet){
+            b += 150;
+            g += 150;
+        }
+
+
         fill(color(r, g, b));
         noStroke();
         rect(this.x * w, this.y * h, w, h);
     };
 
+    /*Função que gera o mapa*/
     this.setDifficulty = function (initialDif) {
-        var maxValue = abs(heuristic(start, end)/8);
+        var maxValue = abs(heuristic(start, end, choosenDistanceMethod)/8);
         this.difficulty = floor(initialDif);
 
         var tempArr = shuffle(this.neighbors);
@@ -137,6 +222,7 @@ function Cell(xpos, ypos){
 
     };
 
+    /*Função que calcula para cada nó quais são seus vizinhos*/
     this.addNeibors = function (grd, isDiagonal) {
         this.neighbors = [];
 
@@ -176,7 +262,8 @@ function Cell(xpos, ypos){
     };
 }
 
-
+/*Função de setup do algoritmo*/
+var stepsText;
 function setup() {
     randomSeed(0);
     var mycanvas = createCanvas(600, 600);
@@ -193,19 +280,21 @@ function setup() {
     var resetButton = createButton("Reset");
 
     beginButton.mousePressed(begin);
-    resetButton.mousePressed(reset)
+    resetButton.mousePressed(reset);
+
+    //stepsText = createP('Steps: 0');
 }
 
-var execute = false;
-
+/*Função que dispara o início da busca*/
 function begin() {
     execute = true;
 }
 
+/*Função que limpa as estruturas de dados geradas e prepara uma nova execução*/
 function reset() {
     var size = inputSize.value();
 
-    if(size > 3 && size <= 200){
+    if(size > 3 && size <= 110){
         cols = size;
         rows = size;
     }
@@ -223,7 +312,7 @@ function reset() {
     loop();
 }
 
-
+/*Função que gera o grid colocando e conectando os nós*/
 function generateGrid() {
     grid = new Array(cols);
     //Fazendo uma matriz bidimencional
@@ -246,26 +335,29 @@ function generateGrid() {
     end.setDifficulty(0.1);
 
     calculateNeigbours();
-    calculateNeigbours();
+    //calculateNeigbours();
 
 
     //Colocando o ponto inicial na lista de nós abertos
     openSet.push(start);
     //console.log(grid);
 
-    start.wall = false;
-    end.wall = false;
 
+
+
+    //Vizinhos dos e nós inicial e final não podem ser barreiras
+    end.wall = false;
     for(var i = 0; i < end.neighbors.length; i++){
         end.neighbors[i].wall = false;
     }
 
+    start.wall = false;
     for(var i = 0; i < start.neighbors.length; i++){
         start.neighbors[i].wall = false;
     }
 }
 
-
+/*Função para definir os vizinhos de cada nó*/
 function calculateNeigbours() {
     for(var i = 0; i < cols; i++){
         for(var j = 0; j < rows; j++){
@@ -274,15 +366,22 @@ function calculateNeigbours() {
     }
 }
 
+
+/*Função que desenha a interface: essa função é chamada repetidamente pelo
+* framework utilizado, ela é utilizada ao invés de uma estrutura de repetição,
+* dessa forma cada etapa do algoritmo será desenhada na tela*/
 function draw() {
     background(0);
 
+    /*Se a flag que indica que o algoritmo deve executar for ativada*/
     if(execute){
+        stepsCount ++;
         if(openSet.length > 0){
-            //Continuar procurando
+            //BUSCANDO
 
             var lowestIndex = 0;
 
+            //Encontrando o nó com menor custo (f)
             for(var i = 0; i < openSet.length; i++){
                 if(openSet[i].f < openSet[lowestIndex].f){
                     lowestIndex = i;
@@ -290,14 +389,18 @@ function draw() {
             }
             var current = openSet[lowestIndex];
 
+            //Caso tenha chegado ao fim
             if(current === end){
                 console.log("DONE!");
                 noLoop();
             }
 
+
             removeFromArray(openSet, current);
             closeSet.push(current);
+            current.inClosedSet = true;
 
+            //Definindo variáveis para desenhar o caminho atual
             path = [];
             var temp = current;
             path.push(temp);
@@ -306,15 +409,18 @@ function draw() {
                 temp = temp.previous;
             }
 
+            //Cálculo da heurística e definição do caminho
             var neighbors = current.neighbors;
-
             for(var i = 0; i < neighbors.length; i++){
                 var neighbour = neighbors[i];
 
                 var newPath = false;
+                //Caso o vizinho em questão não esteja fechado e não seja uma barreira
                 if(!closeSet.includes(neighbour) && !neighbour.wall){
+                    //Cálculo de um g(n) temporário
                     var tempg = current.g + neighbour.difficulty;
 
+                    //Escolh do menor g
                     if(openSet.includes(neighbour)){
                         if(tempg < neighbour.g){
                             neighbour.g = tempg;
@@ -326,16 +432,17 @@ function draw() {
                         newPath = true;
                     }
                     if(newPath){
-                        neighbour.h = heuristic(neighbour, end);
+                        //Cálculo da heurística
+                        neighbour.h = heuristic(neighbour, end, choosenDistanceMethod);
                         neighbour.f = (m * neighbour.g) + (n * neighbour.h);
                         neighbour.previous = current;
                     }
                 }
             }
         }else{
+            //NÃO EXISTE SOLUÇÃO
             console.log('No solution');
             noLoop();
-            //return;
         }
     }
 
@@ -346,21 +453,22 @@ function draw() {
         }
     }
 
-    /*for(var i = 0; i < closeSet.length; i++){
-        closeSet[i].showPath(color(255,0,0));
+    for(var i = 0; i < closeSet.length; i++){
+        closeSet[i].inClosedSet = true;
+        closeSet[i].inOpenSet = false;
     }
-    */
     for(var i = 0; i < openSet.length; i++){
-        openSet[i].showPath(color(132, 132, 132));
+        //openSet[i].showPath(color(132, 132, 132));
+        openSet[i].inOpenSet = true;
     }
 
     for(var i = 0; i < path.length; i++){
-        path[i].showPath(color(89, 65, 65));
+        path[i].showPath(color(49, 0, 198));
     }
 
 }
 
-
+/*Função que embaralha os elementos de um vetor*/
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
